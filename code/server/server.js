9947cf92d724e2807067899c8e814b8fae8f988f
Trygve Laugstøl <trygvis@inamo.no>
2012-04-20 11:37:17 +0200

    wip

--- code/server/server.js
+++ code/server/server.js
@@ -1,21 +1,60 @@
 var express = require('express')
   , app = express.createServer()
-  , routes = require('./lib/routes')
   , mongoose = require('mongoose')
-  , Db = require('mongodb').Db
-  , Server = require('mongodb').Server;
+  , Schema = mongoose.Schema;
+
+var Ad = new Schema({
+    title : String
+  , body  : String
+});
+var Db = {
+  Ad: mongoose.model('Ad', Ad)
+};
+
+function new_ad(req, res) {
+  console.log("req.body", req.body);
+  var ad = new Db.Ad();
+  ad.title = req.body.title;
+  ad.body = req.body.body;
+  ad.save();
+
+  var json = {
+    result: 'ok',
+    ad: ad
+  };
+
+  res.send(JSON.stringify(json), {'Content-Type': 'application/json'}, 200);
+};
+
+function get_ad(req, res) {
+  console.log("req.params.id", req.params.id);
+  Db.Ad.findOne({_id: req.params.id}, function(err, doc) {
+    console.log("err", err);
+    console.log("doc", doc);
+
+    var json;
+    if(doc == null) {
+      json = {
+        result: 'failure',
+        message: 'Unknown object'
+      };
+    }
+    else {
+      json = {
+        result: 'ok',
+        ad: doc
+      };
+    }
+
+    return res.send(JSON.stringify(json), {'Content-Type': 'application/json'}, 200);
+  });
+};
 
 app.configure(function(){
-//  app.set('views', __dirname + '/views');
-//  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.cookieParser('secret'));
   app.use(express.session({ secret: 'secret' }));
   app.use(app.router);
-//  app.use(require("stylus").middleware({
-//    src: __dirname + "/public",
-//    compress: true
-//  }));
   app.use(express.static(__dirname + '/public'));
 });
 
@@ -27,13 +66,9 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-function run() {
-  mongoose.connect('mongodb://localhost/server-1', function() {
-    app.post('/ad', routes.new_ad);
-    app.get('/ad/:id', routes.get_ad);
-    app.listen(8000);
-    console.log("It's on!");
-  });
-}
-
-run();
+mongoose.connect('mongodb://localhost/server-1', function() {
+  app.post('/ad', new_ad);
+  app.get('/ad/:id', get_ad);
+  app.listen(8000);
+  console.log("It's on!");
+});
