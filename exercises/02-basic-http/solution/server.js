1818b471bc97d5854efb2ee63dda7d41712e5aa1
Trygve Laugstøl <trygvis@inamo.no>
2012-05-31 10:51:45 +0200

    o Fixing broken Content-Type+Method parsing. o Adding checks for Allow header.

--- exercises/02-basic-http/solution/server.js
+++ exercises/02-basic-http/solution/server.js
@@ -17,13 +17,13 @@ var Db = {
   Ad: mongoose.model('Ad', Ad)
 };
 
-function assertJson(req, res) {
+function assertContentType(req, res) {
   var ct = req.headers['content-type'] || '';
   if('application/json' == ct.toLowerCase()) {
     return true;
   }
   var txt = 'You have to post application/json';
-  res.writeHead(406, txt, {'Content-Type': 'text/plain'});
+  res.writeHead(415, txt, {'Content-Type': 'text/plain'});
   res.write(txt);
   res.write('\n');
   res.end();
@@ -42,51 +42,74 @@ function assertMethod(req, res, method) {
   return false;
 }
 
+function assertAccept(req, res, method) {
+  console.log('req.headers.accept', typeof req.headers.accept, req.headers.accept);
+  console.log('req.headers.accept', typeof req.headers.accept === 'undefined');
+  if(typeof req.headers.accept === "undefined" ||
+     req.headers.accept == 'application/json' ||
+     req.headers.accept == '*/*') {
+    return true;
+  }
+  var txt = 'Illegal accept, you can only use application/json';
+  res.writeHead(406, txt, {'Content-Type': 'text/plain'});
+  res.write(txt);
+  res.write('\n');
+  res.end();
+  return false;
+}
+
+var seq = 0;
 mongoose.connect('mongodb://localhost/01-http-rpc', function() {
   var db = mongoose.connection.db
   http.createServer(function(req, res) {
-    console.log(req.method + ' ' + req.url);
+    seq++;
+    console.log(seq + ':' + req.method + ' ' + req.url);
+    console.log(seq + ':' + util.inspect(req.headers));
     var u = url.parse(req.url, true);
     if(u.pathname == "/create-ad") {
-      if(assertJson(req, res) && assertMethod(req, res, 'POST')) {
-        var s = "";
-        req.on('data', function(chunk) {
-          s += chunk;
-        });
-        req.on('end', function() {
-          var payload = JSON.parse(s);
-          var ad = new Db.Ad();
-          ad.title = payload.title;
-          ad.body = payload.body;
-          ad.save();
-          console.log(payload);
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          res.write(JSON.stringify({ id: ad._id }));
-          res.end("\n");
-        });
-      }
-    } else if(u.pathname == "/ad") {
-      console.log(u.query, u.query.id);
-      if(assertJson(req, res) && assertMethod(req, res, 'GET')) {
-        console.log('/ad, yo');
-        req.on('end', function() {
-          console.log('end');
-          Db.Ad.findOne({_id: u.query.id}, function(err, doc) {
-            console.log('findOne');
-            if(err) {
-              res.writeHead(500, {'Content-Type': 'text/plain'});
-              res.write(JSON.stringify(err.message));
-            }
-            else if(doc == null) {
-              res.writeHead(404, {'Content-Type': 'text/plain'});
-              res.write("Unknown ad: " + u.query.id);
-            }
-            else {
-              res.write(JSON.stringify({ result: "ok", data: doc}));
-            }
+      if(assertMethod(req, res, 'POST')) {
+        if(assertContentType(req, res)) {
+          var s = "";
+          req.on('data', function(chunk) {
+            s += chunk;
+          });
+          req.on('end', function() {
+            var payload = JSON.parse(s);
+            var ad = new Db.Ad();
+            ad.title = payload.title;
+            ad.body = payload.body;
+            ad.save();
+            console.log(seq + ':' + util.inspect(payload));
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify({ id: ad._id }));
             res.end("\n");
           });
-        });
+        }
+      }
+    } else if(u.pathname == "/ad") {
+      console.log(seq + ': id=' + u.query.id);
+      if(assertMethod(req, res, 'GET')) {
+        if(assertAccept(req, res)) {
+          console.log(seq + ':' + '/ad, yo');
+          req.on('end', function() {
+            console.log(seq + ':' + 'end');
+            Db.Ad.findOne({_id: u.query.id}, function(err, doc) {
+              console.log(seq + ':' + 'findOne');
+              if(err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.write(JSON.stringify(err.message));
+              }
+              else if(doc == null) {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.write("Unknown ad: " + u.query.id);
+              }
+              else {
+                res.write(JSON.stringify({ result: "ok", data: doc}));
+              }
+              res.end("\n");
+            });
+          });
+        }
       }
     } else if(u.pathname == "/add-picture") {
       var failed = false;
--- exercises/02-basic-http/solution/test/test.js
+++ exercises/02-basic-http/solution/test/test.js
@@ -17,42 +17,61 @@ function setId(outgoing) {
 suite.
   use('localhost', 3000).
   before('setId', setId).
-  discuss('Create Ad endpoint: ').
-    path('/create-ad').
-    setHeader('Content-Type', 'application/json').
-    post({ title: 'Fin bolig til salgs!', body: 'Fire rom, nytt bad.' }).
-    expect(200).
-    expect('body should include id key', function (err, res, body) {
-      var body = JSON.parse(body);
-      assert.ok(body.id);
-      id = body.id;
-    }).
-    get().
-    expect(405).
-    expect('response should include Allow header', function (err, res, body) {
-      assert.equal('Illegal method, you can only use: POST\n', body);
-    }).
-    expect('body should describe error message', function (err, res, body) {
-      assert.equal('Illegal method, you can only use: POST\n', body);
-    }).
-  undiscuss().unpath().
+  discuss('Create Ad endpoint,').
+    discuss('valid create').
+      setHeader('Content-Type', 'application/json').
+      post('/create-ad', { title: 'Fin bolig til salgs!', body: 'Fire rom, nytt bad.' }).
+      expect(200).
+      expect('body should include id key', function (err, res, body) {
+        var body = JSON.parse(body);
+        assert.ok(body.id);
+        id = body.id;
+      }).
+      removeHeader('Content-Type').
+    undiscuss().
+    discuss('invalid method').
+      get('/create-ad').
+      expect(405).
+      expect('response should include Allow header', function (err, res, body) {
+        assert.equal(res.headers['allow'], 'POST');
+      }).
+      expect('body should describe error message', function (err, res, body) {
+        assert.equal('Illegal method, you can only use: POST\n', body);
+      }).
+    undiscuss().
+    discuss('with \'Content-Type: fjas\'').
+      setHeader('Content-Type', 'fjas').
+      post('/create-ad').
+      expect(415).
+      removeHeader('Content-Type').
+    undiscuss().
+  undiscuss().
   next().
   discuss('Ad endpoint').
-    get('/ad').
-    expect(200).
-  undiscuss().unpath().
+    discuss('With \'Accept: application/json\'').
+      setHeader('Accept', 'application/json').
+      get('/ad').
+      expect(200).
+      removeHeader('Accept').
+    undiscuss().
+    discuss('With \'Accept: fjas\'').
+      setHeader('Accept', 'fjas').
+      get('/ad').
+      expect(406).
+      removeHeader('Accept').
+    undiscuss().
+  undiscuss().
   next().
   discuss('Generic 404').
-    path('/not-found').
-    removeHeader('Content-Type').
-    get().
+    get('/not-found').
     expect(404).
     expect('body should include id key', function (err, res, body) {
       assert.equal(body, 'Not found\n');
     }).
-    post().
+    post('/not-found').
     expect(404).
     expect('body should include id key', function (err, res, body) {
       assert.equal(body, 'Not found\n');
     }).
+  undiscuss().
 export(module);
